14c82a12597a34106a043065f9b33d260c9791a4
piperal <piperal@tlu.ee>
2026-02-13 10:57:07 +0200

    film

--- fimimudel/src/main/java/ee/piperal/fimimudel/controller/MovieController.java
+++ fimimudel/src/main/java/ee/piperal/fimimudel/controller/MovieController.java
@@ -3,10 +3,8 @@ package ee.piperal.fimimudel.controller;
 import ee.piperal.fimimudel.entity.Movie;
 import ee.piperal.fimimudel.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -20,13 +18,13 @@ public class MovieController {
         return movieRepository.findAll();
     }
 
-    @GetMapping("movies/{id}")
+    @DeleteMapping("movies/{id}")
     public List<Movie> delMovie(@PathVariable Long id){
         movieRepository.deleteById(id);
         return movieRepository.findAll();
     }
 
-    @GetMapping("movies/add")
+    @PostMapping("movies/add")
     public List<Movie> addMovie(@RequestParam Movie movie){
         movieRepository.save(movie);
         return movieRepository.findAll();
